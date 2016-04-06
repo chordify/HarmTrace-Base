aa5857fd6b46c2bfe4a88c6e9d076499d368e756
Bas de Haas <bas@chordify.net>
2016-04-06 17:41:47 +0200

    playing around with new Timed data type, making the timestamp more general. N.B. does not compile

--- src/HarmTrace/Base/Time.hs
+++ src/HarmTrace/Base/Time.hs
@@ -9,38 +9,38 @@
 -- Copyright   :  (c) 2012--2015 W. Bas de Haas and Jose Pedro Magalhaes
 -- License     :  LGPL-3
 --
--- Maintainer  :  bas@chordify.net, dreixel@chordify.net 
+-- Maintainer  :  bas@chordify.net, dreixel@chordify.net
 -- Stability   :  experimental
 -- Portability :  non-portable
 --
 -- Summary: A set of types and classes for representing musical time, mainly
--- (but not necessarily) in the context of recognising chords 
+-- (but not necessarily) in the context of recognising chords
 -- from an arbitrary audio source.
 --------------------------------------------------------------------------------
 
 module HarmTrace.Base.Time (
-   
+
    NumData
   -- ** Representing musical time
   , Timed (..)
   , Beat (..)
-  , BeatTime (..) 
+  , BeatTime (..)
   , MeterKind (..)
 
   -- * Functions
   -- ** Data access
   , timed
   , timedBT
-  , onBeatTime 
-  , offBeatTime 
-  , onBeat 
-  , offBeat 
+  , onBeatTime
+  , offBeatTime
+  , onBeat
+  , offBeat
   , onset
   , offset
   , duration
   , setData
   , getEndTime
-  
+
   -- ** Type conversion and other utilities
   -- , fromDurations
   , mergeTimed
@@ -52,48 +52,49 @@ module HarmTrace.Base.Time (
   , updateBeats
   , splitPickup -- remove
   , nextBeat
-  , prevBeat 
+  , prevBeat
   , lastBeat
   , dropTimed
   , timeStamp
   , timeComp
   , roundingError
-  , beat 
+  , beat
   , pprint
   , prettyPrint
 
 ) where
-             
+
 import Data.List                      ( intercalate, mapAccumL )
 import Text.Printf                    ( printf )
 
 -- | When reducing and expanding 'Timed' types there might be rounding
 -- errors in the floating point time stamps. The 'roundingError' parameter
--- sets the acceptable rounding error that is used in the comparison of 
+-- sets the acceptable rounding error that is used in the comparison of
 -- time stamps (e.g. see 'timeComp')
-roundingError :: NumData 
-roundingError = 0.0001 -- = one millisecond  
+roundingError :: BeatTime
+roundingError = 0.0001 -- = one millisecond
 
 
--- | A type synonym is defined for our main numerical representation, this 
+-- | A type synonym is defined for our main numerical representation, this
 -- allows us to easily change the precision.
-type NumData = Double
+-- type NumData = Float
 
 --------------------------------------------------------------------------------
 -- High-level structure
 --------------------------------------------------------------------------------
 
+type Timed a = Fractional t => Timed' a t
 
--- | A datatype that wraps around an (musical) datatype, adding information 
--- about the musical time to this datatype. Musical time is stored as 
+-- | A datatype that wraps around an (musical) datatype, adding information
+-- about the musical time to this datatype. Musical time is stored as
 -- a list of 'BeatTime' time stamps that can optionally be augmented
--- with information about the 'Beat' position of the particular time stamp 
+-- with information about the 'Beat' position of the particular time stamp
 -- inside the bar.
-data Timed a = Timed { -- | Returns the contained datatype 
-                               getData :: a 
-                               -- | Returns the list of TimeStamps
-                             , getTimeStamps :: [BeatTime]
-                             } deriving (Functor, Show, Eq)
+data Timed' a t = Timed { getData :: a
+                        -- | Returns the contained datatype
+                        , getTimeStamps :: [BeatTime t]
+                        -- | Returns the list of TimeStamps
+                        } deriving (Functor, Show, Eq)
 
 -- | For now, we fix the number of available beats to four, because this is also
 -- hard-coded into the bar and beat-tracker.
@@ -107,7 +108,7 @@ data MeterKind = Duple | Triple deriving (Eq, Show, Ord)
 -- Instances of high-level data structure
 --------------------------------------------------------------------------------
 
-  
+
 instance Show Beat where
   show One   = "1"
   show Two   = "2"
@@ -115,21 +116,21 @@ instance Show Beat where
   show Four  = "4"
   show NoBeat = "x"
 
-instance Show BeatTime where
+instance Fractional t => Show (BeatTime t) where
   show (BeatTime t bt) = printf ("(%.2f, " ++ show bt ++ ")") t
   show (Time t)        = printf  "(%.2f)" t
 
-    
+
 --------------------------------------------------------------------------------
 -- numerical data representation
 --------------------------------------------------------------------------------
 
+-- type BeatTime = BeatAndTime Float
 
--- TODO Rename to BeatTime
 -- | Represents a musical time stamp, which is a 'NumData' possibly augmented
 -- with a 'Beat' denoting the position of the time stamp within a bar.
-data BeatTime = BeatTime NumData Beat
-              | Time     NumData      deriving Eq
+data BeatTime a = BeatTime a Beat
+                | Time     a       deriving Eq
 
 -- we compare based on the timestamp only
 instance Ord BeatTime where
@@ -148,36 +149,36 @@ fromDurations z td = foldl' step [] td where
 -}
 
 -- | alternative 'Timed' constructor
-timed :: a -> NumData -> NumData -> Timed a
+timed :: Fractional t => a -> t -> t -> Timed a
 timed d x y = Timed d [Time x, Time y]
 
 -- | alternative 'Timed' constructor
 timedBT :: a -> BeatTime -> BeatTime -> Timed a
 timedBT d x y = Timed d [x, y]
 
--- | concatenates the 'BeatTime' timestamps of two 'Timed's and 
--- creates a new 'Timed' that stores the first argument. 
+-- | concatenates the 'BeatTime' timestamps of two 'Timed's and
+-- creates a new 'Timed' that stores the first argument.
 -- N.B. this function uses 'timeComp' to allow for very small timing deviations
 concatTimed :: a -> Timed a -> Timed a -> Timed a
-concatTimed dat (Timed _ ta) (Timed _ tb) = 
+concatTimed dat (Timed _ ta) (Timed _ tb) =
   Timed dat (mergeBeatTime ta tb) where
 
   mergeBeatTime :: [BeatTime] -> [BeatTime] -> [BeatTime]
   mergeBeatTime [] b = b
   mergeBeatTime a [] = a
-  mergeBeatTime a b = case timeComp (timeStamp . last $ a) 
+  mergeBeatTime a b = case timeComp (timeStamp . last $ a)
                                     (timeStamp . head $ b) of
     GT -> error ("HarmTrace.Base.MusicTime.mergeBeatTime: " ++
                  "cannot merge BeatTimes "  ++ show a ++ " and " ++ show b)
     EQ -> a ++ tail b -- do not include the same timestamp twice
-    LT -> a ++ b  
+    LT -> a ++ b
 
 
 -- | the inverse of 'mergeTimed', expanding the list 'Timed' elements to all
--- timestamps stored in the 'getTimeStamps' list. N.B. 
+-- timestamps stored in the 'getTimeStamps' list. N.B.
 --
 -- >>> expandTimed (mergeTimed x) = x :: [Timed a]
--- 
+--
 -- also,
 --
 -- >>> (expandTimed cs) = cs
@@ -191,7 +192,7 @@ expandTimed :: [Timed a] -> [Timed a]
 expandTimed = concatMap replic where
 
   replic :: Timed a -> [Timed a]
-  replic (Timed c ts) = let reps = repeat c 
+  replic (Timed c ts) = let reps = repeat c
                         in  zipWith3 timedBT (c : reps) ts (tail ts)
 
 -- | merges consecutive 'Timed' values that store the same element (using
@@ -201,11 +202,11 @@ expandTimed = concatMap replic where
 -- >>> [Timed {getData = "c", getTimeStamps = [(0.0),(1.0),(2.0)]}
 -- >>> ,Timed {getData = "d", getTimeStamps = [(3.0),(4.0),(5.0)]}
 -- >>> ,Timed {getData = "e", getTimeStamps = [(5.0),(6.0)]}]
--- 
+--
 mergeTimed :: Eq a => [Timed a] -> [Timed a]
 mergeTimed = mergeTimedWith (==)
 
--- | Does exactly what 'mergeTimed' does, but allows for a custom equality 
+-- | Does exactly what 'mergeTimed' does, but allows for a custom equality
 -- function
 mergeTimedWith :: forall a. Eq a => (a -> a -> Bool) -> [Timed a] -> [Timed a]
 mergeTimedWith eq = foldr groupT [] where
@@ -222,32 +223,32 @@ mergeTimedWith eq = foldr groupT [] where
 -- >>> splitTimed (Timed "x" [Time 2, Time 5]) 4
 -- >>> ( Timed {getData = "x", getTimeStamps = [(2.0),(4.0)]}
 -- >>> , Timed {getData = "x", getTimeStamps = [(4.0),(5.0)]} )
-splitTimed :: Show a => Timed a -> NumData -> (Timed a, Timed a)
-splitTimed td@(Timed d t) s 
+splitTimed :: (Show a, Fractional t) => Timed a t -> t -> (Timed a, Timed a)
+splitTimed td@(Timed d t) s
   | s > onset td = case span ((< s) . timeStamp) t of
                     (_, []) -> e
                     (x, y ) -> ( Timed d (x ++ [Time s])
                                , Timed d (Time s : y   ))
   | otherwise    = e
-      where e = error ( "HarmTrace.Base.MusicTime.splitTimed: Timestamp " 
-                      ++ show s ++ " not in range of Timed: " ++ show td) 
+      where e = error ( "HarmTrace.Base.MusicTime.splitTimed: Timestamp "
+                      ++ show s ++ " not in range of Timed: " ++ show td)
 
 -- | Changes the internal 'MeterKind' of a 'Timed' sequence. We assume
 -- that meter changes do nog occur.
 setMeterKind :: MeterKind -> [Timed a] -> [Timed a]
 setMeterKind _ [] = []
-setMeterKind mk x = 
+setMeterKind mk x =
   let (pu, cs) = splitPickup x -- will expand the chords
-  
+
       srtpu    = (iterate (prevBeat mk) (lastBeat mk)) !! (pred . length $ pu)
       srt      = onBeat (head cs)
-      
+
   in  (updateBeats mk srtpu pu) ++
       (updateBeats mk srt   cs)
 
 -- TODO: maybe setBeats is a better name..?
 -- | applies updateBeat to a list
--- 
+--
 -- >>> updateBeats Triple Three [ timedBT "a" (BeatTime 0 Three) (BeatTime 1 Four)
 -- >>>                          , timedBT "a" (BeatTime 1 Four) (BeatTime 2 One)
 -- >>>                          , Timed "a" [ BeatTime 2 One, BeatTime 3 Two
@@ -259,36 +260,36 @@ updateBeats :: MeterKind -> Beat -> [Timed a] -> [Timed a]
 updateBeats _      _      [] = []
 updateBeats Triple Four   cs = updateBeats Triple One cs
 updateBeats _      NoBeat cs = cs
-updateBeats mk s cs = snd . mapAccumL f s $ cs 
+updateBeats mk s cs = snd . mapAccumL f s $ cs
 
   where f :: Beat -> Timed a -> (Beat, Timed a)
-        f a b = let x = updateBeat mk a b in (offBeat x, x) 
+        f a b = let x = updateBeat mk a b in (offBeat x, x)
 
--- | Update the 'Beat's in 'Timed' data given a 'MeterKind' and a 
+-- | Update the 'Beat's in 'Timed' data given a 'MeterKind' and a
 -- starting beat:
--- 
--- >>> updateBeat Triple Two (Timed "c" [ BeatTime 0 Three 
+--
+-- >>> updateBeat Triple Two (Timed "c" [ BeatTime 0 Three
 -- >>>                                  , BeatTime 1 Four
 -- >>>                                  , BeatTime 2 One])
 -- >>> Timed {getData = "c", getTimeStamps = [(0.0, 2),(1.0, 3),(2.0, 1)]}
 updateBeat :: MeterKind -> Beat -> Timed a -> Timed a
-updateBeat mk strt (Timed d ts) = 
-  Timed d . zipWith BeatTime (map timeStamp ts) 
+updateBeat mk strt (Timed d ts) =
+  Timed d . zipWith BeatTime (map timeStamp ts)
           . iterate (nextBeat mk) $ strt
 
--- | 
+-- |
 -- N.B. calls 'expandTimed' before splitting
 splitPickup :: [Timed a] -> ([Timed a], [Timed a])
 splitPickup cs = case span (\t -> (onBeat t) /= One) . expandTimed $ cs of
-                  (x, []) -> ([], x) -- in case we have a very short sequence 
+                  (x, []) -> ([], x) -- in case we have a very short sequence
                                      -- don't treat it as a pickup
                   y       -> y
 
-    
+
 -- | compares to 'NumData' timestamps taking a rounding error 'roundingError'
 -- into account.
-timeComp :: NumData -> NumData -> Ordering
-timeComp a b 
+timeComp :: Fractional t => t -> t -> Ordering
+timeComp a b
  | a > (b + roundingError) = GT
  | a < (b - roundingError) = LT
  | otherwise               = EQ
@@ -298,9 +299,9 @@ setData :: Timed a -> b -> Timed b
 setData td d = td {getData = d}
 
 -- | Returns the 'NumData' timestamp, given a 'BeatTime'
-timeStamp :: BeatTime -> NumData
+timeStamp :: Fractional t -> BeatTime -> t
 timeStamp (BeatTime t _bt) = t
-timeStamp (Time     t    ) = t  
+timeStamp (Time     t    ) = t
 
 -- | Returns the 'NumData' timestamp, given a 'BeatTime'
 beat :: BeatTime -> Beat
@@ -317,62 +318,62 @@ onBeatTime td = case getTimeStamps td of
 offBeatTime :: Timed a -> BeatTime
 offBeatTime td = case getTimeStamps td of
   []  -> error "HarmTrace.Base.Time.offBeatTime: no timestamps are stored"
-  l   -> last $ l
+  l   -> last l
 
 -- | Returns the start 'Beat'
 onBeat :: Timed a -> Beat
-onBeat = beat . onBeatTime 
+onBeat = beat . onBeatTime
 
 -- | Returns the offset time stamp
 offBeat :: Timed a -> Beat
 offBeat = beat . offBeatTime
 
 -- | Returns the onset time stamp
-onset :: Timed a -> NumData
-onset = timeStamp . onBeatTime 
+onset :: Fractional t => Timed a -> t
+onset = timeStamp . onBeatTime
 
 -- | Returns the offset time stamp
-offset :: Timed a -> NumData
-offset = timeStamp . offBeatTime 
+offset :: Fractional t => Timed a -> t
+offset = timeStamp . offBeatTime
 
 -- | Given a list of 'Timed' values, returns the end time of the latest element
 -- in the list.
-getEndTime :: [Timed a] -> NumData
+getEndTime :: Fractional t => [Timed a] -> t
 getEndTime [] = error "getEndTime: empty list"
 getEndTime l  = offset . last $ l
 
 -- | Returns the duration of 'Timed'
-duration :: Timed a -> NumData
+duration :: Fractional t => Timed a -> t
 duration td = offset td - onset td
 
 -- TODO: replace by ad-hoc enum instance?
--- | returns the next beat, e.g. @ nextBeat Two = Three @. 
--- Following the (current) definition of 'Beat', we still assume 4/4, in the 
--- future this function should also have the meter as an argument. 
+-- | returns the next beat, e.g. @ nextBeat Two = Three @.
+-- Following the (current) definition of 'Beat', we still assume 4/4, in the
+-- future this function should also have the meter as an argument.
 -- N.B. @ nextBeat Four = One @
-nextBeat :: MeterKind -> Beat -> Beat 
+nextBeat :: MeterKind -> Beat -> Beat
 nextBeat Duple  Four   = One
 nextBeat Triple Three  = One
 nextBeat _      NoBeat = error "HarmTrace.Base.Time.nextBeat: nextBeat applied toNoBeat"
 nextBeat _      b      = succ b
 
 -- | returns the previous 'Beat', similar to 'prevBeat'.
-prevBeat :: MeterKind -> Beat -> Beat 
+prevBeat :: MeterKind -> Beat -> Beat
 prevBeat Duple  One  = Four
 prevBeat Triple One  = Three
 prevBeat _      NoBeat = error "HarmTrace.Base.Time.prevBeat: nextBeat applied toNoBeat"
 prevBeat _      b    = pred b
 
--- | returns the last 'Beat' of the 'MeterKind' 
--- 
--- >>> lastBeat Duple 
+-- | returns the last 'Beat' of the 'MeterKind'
+--
+-- >>> lastBeat Duple
 -- >>> Four
 lastBeat :: MeterKind -> Beat
 lastBeat Triple = Three
 lastBeat Duple  = Four
 
--- | drops the time (with or without 'Beat') information of a list 
--- 'Timed' data structure 
+-- | drops the time (with or without 'Beat') information of a list
+-- 'Timed' data structure
 dropTimed :: [Timed a] -> [a]
 dropTimed = map getData
 
@@ -384,5 +385,5 @@ prettyPrint = intercalate "\n" . map pprint where
 pprint :: Show a => Timed a -> String
 pprint (Timed d [ ]) = "not set - not set: " ++ show d
 pprint (Timed d [x]) = show x ++" - not set: " ++ show d
-pprint (Timed d ts ) = show (head ts) ++ " - " ++ show (last ts) 
+pprint (Timed d ts ) = show (head ts) ++ " - " ++ show (last ts)
                                       ++ ": "  ++ show d
